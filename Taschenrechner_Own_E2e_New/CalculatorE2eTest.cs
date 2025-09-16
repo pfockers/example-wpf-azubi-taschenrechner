6a3563814de77f0e5a22c160d6f161408a603d8d
Philipp Fockers <pfockers@rosen-group.com>
2025-09-16 14:23:01 +0200

    Added e2e project with test

---
+++ Taschenrechner_Own_E2e_New/CalculatorE2eTest.cs
@@ -0,0 +1,43 @@
+﻿// <copyright company="ROSEN Swiss AG">
+//  Copyright (c) ROSEN Swiss AG
+//  This computer program includes confidential, proprietary
+//  information and is a trade secret of ROSEN. All use,
+//  disclosure, or reproduction is prohibited unless authorized in
+//  writing by an officer of ROSEN. All Rights Reserved.
+// </copyright>
+
+namespace Taschenrechner_Own_E2e_New
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using SmartBear.TestLeft;
+    using SmartBear.TestLeft.TestObjects;
+    using SmartBear.TestLeft.TestObjects.WPF;
+
+    [TestClass]
+    public class CalculatorE2eTest
+    {
+        #region Methods
+
+        [TestMethod]
+        public void Addition_TwoPlusThree_ShowsFive()
+        {
+            // MainWindow finden
+            var driver = new LocalDriver();
+            var mainWindow = driver.Find<IWindow>(new WPFPattern());
+
+            // Buttons klicken: 2 + 3 =
+            mainWindow.Find<IButton>(new WPFPattern { WPFControlText = "2" }).Click();
+            mainWindow.Find<IButton>(new WPFPattern { WPFControlText = "+" }).Click();
+            mainWindow.Find<IButton>(new WPFPattern { WPFControlText = "3" }).Click();
+            mainWindow.Find<IButton>(new WPFPattern { WPFControlText = "=" }).Click();
+
+            // Ergebnis auslesen
+            var resultBox = mainWindow.Find<IControl>(new WPFPattern { WPFControlName = "ErgebnisTextBox" });
+            string result = resultBox.ToString();
+
+            Assert.AreEqual("5", result);
+        }
+
+        #endregion
+    }
+}
